819f9c9f4b36c0e287327f722c81046e611e08f4
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-07 04:47:58 +0600

    Add Guest account.

--- lib/features/bottom_navbar/controller/bottom_navbar_controller.dart
+++ lib/features/bottom_navbar/controller/bottom_navbar_controller.dart
@@ -1,7 +1,19 @@
 import 'package:get/get.dart';
+import 'package:jsulima/core/services/shared_preferences_helper.dart';
 
 class BottomNavbarController extends GetxController {
   var selectedIndex = 0.obs;
+  var token = RxnString();
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadToken();
+  }
+
+  Future<void> loadToken() async {
+    token.value = await SharedPreferencesHelper.getAccessToken();
+  }
 
   void changeIndex(int index) {
     selectedIndex.value = index;
